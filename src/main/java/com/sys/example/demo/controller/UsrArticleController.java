304064fd7272ec3324e1c95fa1d1518614fd265f
shimys65 <shimys64@hanmail.net>
2022-04-04 16:58:44 +0900

    ResultData.java 만들고 getArticle에 적용하기

--- src/main/java/com/sys/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/sys/example/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sys.example.demo.service.ArticleService;
+import com.sys.example.demo.util.Ut;
 import com.sys.example.demo.vo.Article;
+import com.sys.example.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -36,13 +38,13 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticleAction(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
-			return id + "번 게시물이 없음";
+			return ResultData.from("F-1", (String) Ut.f("%d번 게시물이 없음", id));
 		}
-		return article;
+		return ResultData.from("S-1", (String) Ut.f("%d번 게시물", id), article);
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
--- src/main/java/com/sys/example/demo/util/Ut.java
+++ src/main/java/com/sys/example/demo/util/Ut.java
@@ -3,16 +3,18 @@ package com.sys.example.demo.util;
 public class Ut {
 
 	public static boolean empty(Object obj) {
+// null이면 부른곳으로 true 리턴
 		if(obj == null) {
 			return true;
 		}
 		
+// null이 아니면 string 확인 후 string 아니면 부른곳으로 true 리턴	
 		if(obj instanceof String == false) {
 			return true;
 		}
 		
-		String str = (String)obj;
-		
+// 둘다 통과 시, 내용 받아서(예:user1) 공백과 길이 검사		
+		String str = (String)obj;		
 		return str.trim().length() == 0;
 	}
 
